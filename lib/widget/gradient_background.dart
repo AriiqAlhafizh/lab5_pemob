60c2b4792c01c2859c8da283ac574035a714cfef
AriiqAlhafizh <ariiqalhafizh69@gmail.com>
2025-09-24 08:52:10 +0700

    first commit

---
+++ lib/widget/gradient_background.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GradientBackground extends StatelessWidget{
+  const GradientBackground({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+
+}
